796a8e5aae04d284576005433e35cf335ea623b9
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-02-28 12:50:25 +0600

    Initial Commit

---
+++ AnimatedText/AnimatedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTextApp.swift
+//  AnimatedText
+//
+//  Created by Rezaul Islam on 28/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
