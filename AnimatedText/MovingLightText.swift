9c91632f5ac86f3d37c2a86e6477a376517c65e5
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2024-02-29 10:39:36 +0600

    Animated Text

---
+++ AnimatedText/MovingLightText.swift
@@ -0,0 +1,18 @@
+//
+//  MovingLightText.swift
+//  AnimatedText
+//
+//  Created by Rezaul Islam on 28/2/24.
+//
+
+import SwiftUI
+
+struct MovingLightText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovingLightText()
+}
